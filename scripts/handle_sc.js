674be73140df6354da7ebe86dcb7418f758e4faa
xrmuseum <virtualmuseum79@gmail.com>
2021-01-23 02:05:34 -0500

    add sc surprisingly good use of api

--- scripts/handle_sc.js
+++ scripts/handle_sc.js
@@ -1,21 +1,60 @@
+SC.initialize({client_id:'b8f06bbb8e4e9e201f9e6e46001c3acb'}); 
+// D7YkmhAjzaV0qsA9e71yKXufTMyJAX2Q
+
+let track_name = document.getElementById("track_name");
 let play_btn = document.getElementById("sc_play");
 let next_btn = document.getElementById("sc_next");
 let prev_btn = document.getElementById("sc_prev");
 
-let current_song = 0;
-let songs = {};
+let play_mode = 0; // paused
+let current_track_num = 3;
+let sc_playlist = null;
+let current_track = null;
 
-function getScData() {
-    songs = document.getElementsByClassName("soundsList__item");
-
-    //prev_btn.addEventListener("click", () => document.getElementById().click());
-    //next_btn.addEventListener("click", () => document.getElementById().click());
-    play_btn.addEventListener("click", handleGetPlay());
+function updateTrackName() {
+    track_name.innerText = sc_playlist.tracks[current_track_num].title;
 }
 
-function handleGetPlay() {
-    document.getElementById("sc_iframe").contentWindow.document.getElementsByClassName("playButton")[0].click();
-    console.log("clicked");
-}
+SC.get('/playlists/1198427683').then(function(playlist) {
+    sc_playlist = playlist;
+    startTrack();
+    updateTrackName();
+});
 
-setTimeout(getScData, 5000);
+play_btn.addEventListener("click", function() {
+    if (play_mode == 0) {
+        play_mode = 1;
+        //console.log(sc_playlist);
+        current_track.play();
+        play_btn.style.backgroundImage = "url('../img/sc_pause_btn.png')";
+    }
+    else {
+        play_mode = 0;
+        current_track.pause();
+        play_btn.style.backgroundImage = "url('../img/sc_play_btn.png')";
+    }
+});
+
+prev_btn.addEventListener("click", function() {
+    if (play_mode == 0) play_btn.click();
+    if (current_track_num == 0) current_track_num = 1;
+    current_track_num--;
+    startTrack();
+    updateTrackName();
+    play_btn.click();
+    setTimeout(() => play_btn.click(), 500);
+});
+
+next_btn.addEventListener("click", function() {
+    if (play_mode == 0) play_btn.click();
+    if (current_track_num == sc_playlist.track_count - 1) current_track_num = -1;
+    current_track_num++;
+    startTrack();
+    updateTrackName();
+    play_btn.click();
+    setTimeout(() => play_btn.click(), 500);
+});
+
+function startTrack() {
+    SC.stream("tracks/"+sc_playlist.tracks[current_track_num].id).then(function(sound) {current_track = sound; current_track.on("finish", () => next_btn.click())});
+}
