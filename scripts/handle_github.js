058c240a9657db6c5d376583cb2fd58fdf1c3004
xrmuseum <virtualmuseum79@gmail.com>
2021-01-24 16:52:06 -0500

    github api

---
+++ scripts/handle_github.js
@@ -0,0 +1,24 @@
+let ghub = document.getElementById("ghub");
+
+async function callMe() {
+    let { Octokit } = await import("https://cdn.skypack.dev/@octokit/core");
+    console.log("fdfs");
+    const octokit = new Octokit({ auth: `90351327fdc32a41dc3618409f010eb3bc19d5ec` });
+
+    console.log('calling');
+    const repos = await octokit.request("GET /users/{org}/repos", {
+        org: "smwllyms"
+    });
+    
+    for (let i = 0; i < repos.data.length; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("ghub_div");
+        console.log(repos.data[i]);
+        newDiv.innerHTML = "<a target='_blank' href='" + repos.data[i].html_url + "'><h3>" + repos.data[i].name + "</h3></a>";
+        newDiv.innerHTML += "<br/>Last pushed on " + repos.data[i].pushed_at.slice(0,10);
+        ghub.appendChild(newDiv);
+    }
+    // expected output: "resolved"
+}
+
+callMe();
